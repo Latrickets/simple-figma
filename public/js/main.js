4474a3147d133edcfff7a4343362ce3c2d5511c3
latrickets <esdrexd.106@gmail.com>
2023-05-25 22:52:21 -0700

    feat: dibujo cuadrado y circulo
    se dibuja el cuadrado y el circulo, falta guardarlos

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+let figuraSeleccionada;
+let figuraArrastrada = false;
+let posX;
+let posY;
+let tamano;
+let figuras = [];
+const divApp = document.getElementById('app');
+
+function setup() {
+    const canvas = createCanvas(divApp.offsetWidth, divApp.offsetHeight);
+    canvas.parent('app');
+
+    // Eventos de mouse para detectar cuando se arrastra el mouse
+    canvas.mousePressed(empezarDibujo);
+    canvas.mouseReleased(terminarDibujo);
+}
+
+function draw() {
+    background(220);
+
+    // Dibujar las figuras guardadas en el arreglo
+    for (let i = 0; i < figuras.length; i++) {
+        let figura = figuras[i];
+        noFill();
+        stroke(0);
+        strokeWeight(2);
+
+        if (figura.tipo === 'circulo') {
+            ellipse(figura.posX, figura.posY, figura.tamano);
+        } else if (figura.tipo === 'rectangulo') {
+            rect(figura.posX, figura.posY, figura.tamano, figura.tamano);
+        }
+    }
+
+    // Dibujar la figura mientras se arrastra el mouse
+    if (figuraArrastrada) {
+        noFill();
+        stroke(0);
+        strokeWeight(2);
+
+        if (figuraSeleccionada === 'circulo') {
+            ellipse(posX, posY, tamano);
+        } else if (figuraSeleccionada === 'rectangulo') {
+            rect(posX, posY, tamano, tamano);
+        }
+    }
+}
+
+function empezarDibujo() {
+    figuraArrastrada = true;
+    posX = mouseX;
+    posY = mouseY;
+    tamano = 0;
+}
+
+function terminarDibujo() {
+    figuraArrastrada = false;
+
+    // Guardar la figura en el arreglo
+    let figura = {
+        tipo: figuraSeleccionada,
+        posX: posX,
+        posY: posY,
+        tamano: tamano,
+    };
+    if (figura.tipo !== '' && figura.tipo !== undefined) figuras.push(figura);
+}
+
+function mouseDragged() {
+    tamano = dist(posX, posY, mouseX, mouseY);
+}
+
+function cambiarFigura() {
+    if (document.getElementById('radioCirculo').checked) {
+        figuraSeleccionada = 'circulo';
+    } else if (document.getElementById('radioRectangulo').checked) {
+        figuraSeleccionada = 'rectangulo';
+    } else if (document.getElementById('radioSelect').checked) {
+        figuraSeleccionada = '';
+    }
+}
--- resources/views/proyecto.blade.php
+++ resources/views/proyecto.blade.php
@@ -5,6 +5,7 @@
     @section('title')
         <title>Proyecto</title>
     @endsection
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
 </head>
 <body>
     {{-- @include('layouts.navbar') --}}
@@ -19,14 +20,14 @@
     </div>
     <nav class="p-4" style="background-color: #161719;">
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-            <label class="btn btn-outline-light tool rounded" for="btnradio1" style="border-top-left-radius: 0; border-bottom-left-radius: 0;"><i class="fas fa-mouse-pointer"></i></label>
+            <input type="radio" class="btn-check" name="btnradio" id="radioSelect" autocomplete="off" checked onchange="cambiarFigura()">
+            <label class="btn btn-outline-light tool rounded" for="radioSelect" style="border-top-left-radius: 0; border-bottom-left-radius: 0;"><i class="fas fa-mouse-pointer"></i></label>
 
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-            <label class="btn btn-outline-light tool rounded" for="btnradio2"><i class="fas fa-square-full"></i></label>
+            <input type="radio" class="btn-check" name="btnradio" id="radioRectangulo" autocomplete="off" onchange="cambiarFigura()">
+            <label class="btn btn-outline-light tool rounded" for="radioRectangulo"><i class="fas fa-square-full"></i></label>
 
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-            <label class="btn btn-outline-light tool rounded" for="btnradio3"><i class="fas fa-circle"></i></label>
+            <input type="radio" class="btn-check" name="btnradio" id="radioCirculo" autocomplete="off" onchange="cambiarFigura()">
+            <label class="btn btn-outline-light tool rounded" for="radioCirculo"><i class="fas fa-circle"></i></label>
 
             <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
             <label class="btn btn-outline-light tool rounded" for="btnradio4"><i class="fas fa-slash fa-rotate-270"></i></label>
@@ -39,12 +40,12 @@
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background-color: #464A4D;">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline my-2">Elementos</span>
                 </a>
             </div>
         </div>
-        <div class="col py-3">
+        <div class="col py-3" id="app">
             
         </div>
         <div class="col-auto col-sm-2 px-0" style="background-color: #464A4D">
@@ -112,4 +113,5 @@
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="{{asset('js/main.js')}}"></script>
 </html>
